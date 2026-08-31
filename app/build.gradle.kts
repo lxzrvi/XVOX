6c84bce40470bd04fa5b538a7966712d5b0cf2e6
lxzrvi <lxzrvi@gmail.com>
2026-08-31 12:42:55 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.xvox.music"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.xvox.music"
+
+        minSdk = 26
+        targetSdk = 36
+
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    val composeBom = platform(
+        "androidx.compose:compose-bom:2025.08.00"
+    )
+
+    implementation(composeBom)
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+}
