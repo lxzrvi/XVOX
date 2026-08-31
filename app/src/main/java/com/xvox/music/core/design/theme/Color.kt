27977f34b9d018cda3696e2ba6900bc0045acea3
lxzrvi <lxzrvi@gmail.com>
2026-08-31 15:32:45 +0530

    Update Color.kt

--- app/src/main/java/com/xvox/music/core/design/theme/Color.kt
+++ app/src/main/java/com/xvox/music/core/design/theme/Color.kt
@@ -2,23 +2,64 @@ package com.xvox.music.core.design.theme
 
 import androidx.compose.ui.graphics.Color
 
-val XvoxPurple = Color(0xFF8B5CF6)
-val XvoxPurpleLight = Color(0xFFA78BFA)
+data class XvoxPalette(
+    val background: Color,
+    val surface: Color,
+    val card: Color,
+    val cardElevated: Color,
+    val cardBorder: Color,
+    val primaryText: Color,
+    val secondaryText: Color,
+    val mutedText: Color,
+    val primaryAccent: Color,
+    val accentSoft: Color,
+    val progressTrack: Color,
+    val progressActive: Color
+)
 
-val XvoxDarkBackground = Color(0xFF101014)
-val XvoxDarkSurface = Color(0xFF1B1B22)
-val XvoxDarkSurfaceVariant = Color(0xFF25252D)
-val XvoxDarkText = Color(0xFFF5F3FF)
-val XvoxDarkSecondaryText = Color(0xFFAAA6B5)
+val XvoxWhitePalette = XvoxPalette(
+    background = Color(0xFFFAFAFA),
+    surface = Color(0xFFFFFFFF),
+    card = Color(0xFFFFFFFF),
+    cardElevated = Color(0xFFF2F2F2),
+    cardBorder = Color(0xFFE2E2E2),
+    primaryText = Color(0xFF111111),
+    secondaryText = Color(0xFF666666),
+    mutedText = Color(0xFF999999),
+    primaryAccent = Color(0xFF171717),
+    accentSoft = Color(0xFFE8E8E8),
+    progressTrack = Color(0xFFD9D9D9),
+    progressActive = Color(0xFF171717)
+)
 
-val XvoxLightBackground = Color(0xFFF8F7FC)
-val XvoxLightSurface = Color(0xFFFFFFFF)
-val XvoxLightSurfaceVariant = Color(0xFFEDEAF4)
-val XvoxLightText = Color(0xFF19171E)
-val XvoxLightSecondaryText = Color(0xFF66616E)
+val XvoxDarkPalette = XvoxPalette(
+    background = Color(0xFF0A0A0A),
+    surface = Color(0xFF111111),
+    card = Color(0xFF171717),
+    cardElevated = Color(0xFF1E1E1E),
+    cardBorder = Color(0xFF292929),
+    primaryText = Color(0xFFF5F5F5),
+    secondaryText = Color(0xFFA3A3A3),
+    mutedText = Color(0xFF666666),
+    primaryAccent = Color(0xFFF5F5F5),
+    accentSoft = Color(0xFF292929),
+    progressTrack = Color(0xFF363636),
+    progressActive = Color(0xFFF5F5F5)
+)
 
-val XvoxAmoledBackground = Color.Black
-val XvoxAmoledSurface = Color(0xFF0B0B0B)
+val XvoxAmoledPalette = XvoxPalette(
+    background = Color(0xFF000000),
+    surface = Color(0xFF050505),
+    card = Color(0xFF0B0B0B),
+    cardElevated = Color(0xFF121212),
+    cardBorder = Color(0xFF202020),
+    primaryText = Color(0xFFFFFFFF),
+    secondaryText = Color(0xFFA1A1A1),
+    mutedText = Color(0xFF5F5F5F),
+    primaryAccent = Color(0xFFFFFFFF),
+    accentSoft = Color(0xFF181818),
+    progressTrack = Color(0xFF303030),
+    progressActive = Color(0xFFFFFFFF)
+)
 
-val XvoxSuccess = Color(0xFF57D38C)
-val XvoxError = Color(0xFFFF6B6B)
+val XvoxSuccess = Color(0xFF45B97C)
