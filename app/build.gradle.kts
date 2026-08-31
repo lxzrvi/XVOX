ffb13938c59041ed58164c512beca2998ce246df
lxzrvi <lxzrvi@gmail.com>
2026-08-31 14:24:10 +0530

    Add lifecycle-viewmodel-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,5 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
 }
