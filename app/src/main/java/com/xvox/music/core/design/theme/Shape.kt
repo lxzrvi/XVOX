d11513427f1f22628859c979b44bba565459f3d5
lxzrvi <lxzrvi@gmail.com>
2026-08-31 14:21:37 +0530

    Add Shape.kt for custom shape definitions

---
+++ app/src/main/java/com/xvox/music/core/design/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.xvox.music.core.design.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val XvoxShapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(20.dp),
+    large = RoundedCornerShape(28.dp)
+)
