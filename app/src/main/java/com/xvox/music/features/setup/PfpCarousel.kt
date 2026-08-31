89e026e07ff7c7a100cd8d3e732a05d0a54e8d6b
lxzrvi <lxzrvi@gmail.com>
2026-08-31 16:58:22 +0530

    Update PfpCarousel.kt

--- app/src/main/java/com/xvox/music/features/setup/PfpCarousel.kt
+++ app/src/main/java/com/xvox/music/features/setup/PfpCarousel.kt
@@ -26,7 +26,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ContentScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
