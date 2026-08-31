affd153d42bdb6d2d12f86fabcfc0255baba57ca
lxzrvi <lxzrvi@gmail.com>
2026-08-31 14:21:06 +0530

    Create Color.kt

---
+++ app/src/main/java/com/xvox/music/core/design/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.xvox.music.core.design.theme
+
+import androidx.compose.ui.graphics.Color
+
+val XvoxPurple = Color(0xFF8B5CF6)
+val XvoxPurpleLight = Color(0xFFA78BFA)
+
+val XvoxDarkBackground = Color(0xFF101014)
+val XvoxDarkSurface = Color(0xFF1B1B22)
+val XvoxDarkSurfaceVariant = Color(0xFF25252D)
+val XvoxDarkText = Color(0xFFF5F3FF)
+val XvoxDarkSecondaryText = Color(0xFFAAA6B5)
+
+val XvoxLightBackground = Color(0xFFF8F7FC)
+val XvoxLightSurface = Color(0xFFFFFFFF)
+val XvoxLightSurfaceVariant = Color(0xFFEDEAF4)
+val XvoxLightText = Color(0xFF19171E)
+val XvoxLightSecondaryText = Color(0xFF66616E)
+
+val XvoxAmoledBackground = Color.Black
+val XvoxAmoledSurface = Color(0xFF0B0B0B)
+
+val XvoxSuccess = Color(0xFF57D38C)
+val XvoxError = Color(0xFFFF6B6B)
