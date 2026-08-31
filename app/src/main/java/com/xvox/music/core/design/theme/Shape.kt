75e211a567884206e9ad9217b94c833322b61531
lxzrvi <lxzrvi@gmail.com>
2026-08-31 15:34:34 +0530

    Update shape corner sizes in Shape.kt

--- app/src/main/java/com/xvox/music/core/design/theme/Shape.kt
+++ app/src/main/java/com/xvox/music/core/design/theme/Shape.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val XvoxShapes = Shapes(
-    small = RoundedCornerShape(12.dp),
-    medium = RoundedCornerShape(20.dp),
-    large = RoundedCornerShape(28.dp)
+    small = RoundedCornerShape(14.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(20.dp)
 )
