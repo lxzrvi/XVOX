6b72869908d94a79c38176d86587a8cf143a171f
lxzrvi <lxzrvi@gmail.com>
2026-08-31 16:49:19 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,5 +38,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
 }
