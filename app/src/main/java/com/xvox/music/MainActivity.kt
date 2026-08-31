be4414a9bd6464ff32f58e29538fd32c1d059f45
lxzrvi <lxzrvi@gmail.com>
2026-08-31 14:26:40 +0530

    Refactor MainActivity to use SetupScreen with theme

--- app/src/main/java/com/xvox/music/MainActivity.kt
+++ app/src/main/java/com/xvox/music/MainActivity.kt
@@ -3,45 +3,28 @@ package com.xvox.music
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.xvox.music.core.design.theme.XvoxTheme
+import com.xvox.music.features.setup.SetupScreen
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(
+            window,
+            false
+        )
+
         setContent {
-            XvoxApp()
-        }
-    }
-}
-
-@Composable
-fun XvoxApp() {
-
-    MaterialTheme {
-
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black),
-            contentAlignment = Alignment.Center
-        ) {
-
-            Text(
-                text = "XVOX",
-                color = Color.White,
-                style = MaterialTheme.typography.displayLarge
-            )
-
+            XvoxTheme {
+                SetupScreen(
+                    onSetupComplete = {
+                        // Navigation to Home comes next.
+                    }
+                )
+            }
         }
     }
 }
