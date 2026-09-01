9331486bd34e0ab1449afe073ca360b37bc73bdc
lxzrvi <lxzrvi@gmail.com>
2026-09-01 16:50:33 +0530

    Refactor HomeScreen to use ViewModel and layout changes

--- app/src/main/java/com/xvox/music/features/home/HomeScreen.kt
+++ app/src/main/java/com/xvox/music/features/home/HomeScreen.kt
@@ -1,27 +1,60 @@
 package com.xvox.music.features.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.xvox.music.core.design.theme.XvoxTheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsState()
     val colors = XvoxTheme.colors
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colors.background),
-        contentAlignment = Alignment.Center
+            .background(colors.background)
+            .windowInsetsPadding(
+                WindowInsets.statusBars
+            )
+            .padding(
+                start = 18.dp,
+                end = 18.dp,
+                top = 10.dp
+            ),
+        verticalArrangement =
+            Arrangement.spacedBy(22.dp)
     ) {
-        Text(
-            text = "XVOX",
-            color = colors.primaryText
-        )
+        if (state.loading) {
+            HomeSkeleton()
+        } else {
+            HomeHeader(
+                profile = state.profile,
+                onRefresh = viewModel::refresh
+            )
+
+            RecentlyPlayedSection(
+                songs = state.recentlyPlayed,
+                onSongClick = viewModel::play
+            )
+
+            AllSongsSection(
+                songs = state.songs,
+                onSongClick = viewModel::play
+            )
+        }
     }
 }
