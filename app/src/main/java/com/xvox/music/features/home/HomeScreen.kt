5f07b376403860c29445b4543ac600c96f7a5db6
lxzrvi <lxzrvi@gmail.com>
2026-09-01 17:00:58 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/xvox/music/features/home/HomeScreen.kt
+++ app/src/main/java/com/xvox/music/features/home/HomeScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -18,42 +20,63 @@ import com.xvox.music.core.design.theme.XvoxTheme
 
 @Composable
 fun HomeScreen(
-    viewModel: HomeViewModel = viewModel()
+    viewModel: HomeViewModel =
+        viewModel()
 ) {
-    val state by viewModel.state.collectAsState()
-    val colors = XvoxTheme.colors
+    val state by
+        viewModel.state.collectAsState()
+
+    val colors =
+        XvoxTheme.colors
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colors.background)
+            .background(
+                colors.background
+            )
             .windowInsetsPadding(
                 WindowInsets.statusBars
+                    .union(
+                        WindowInsets
+                            .navigationBars
+                    )
             )
             .padding(
                 start = 18.dp,
                 end = 18.dp,
-                top = 10.dp
+                top = 10.dp,
+                bottom = 8.dp
             ),
         verticalArrangement =
-            Arrangement.spacedBy(22.dp)
+            Arrangement.spacedBy(
+                20.dp
+            )
     ) {
         if (state.loading) {
             HomeSkeleton()
         } else {
             HomeHeader(
-                profile = state.profile,
-                onRefresh = viewModel::refresh
+                profile =
+                    state.profile,
+                onRefresh =
+                    viewModel::refresh,
+                onMenuClick = {
+                }
             )
 
             RecentlyPlayedSection(
-                songs = state.recentlyPlayed,
-                onSongClick = viewModel::play
+                songs =
+                    state.recentlyPlayed,
+                onSongClick =
+                    viewModel::play
             )
 
             AllSongsSection(
-                songs = state.songs,
-                onSongClick = viewModel::play
+                songs =
+                    state.songs,
+                onSongClick =
+                    viewModel::play
             )
         }
     }
