4ecb5f58cb63de4c16c87a04d557086483af619d
lxzrvi <lxzrvi@gmail.com>
2026-09-01 16:32:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,12 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
