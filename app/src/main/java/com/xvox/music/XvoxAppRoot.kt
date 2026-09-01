848adaccb367bea1733b54e6bcb864ea460bf36f
lxzrvi <lxzrvi@gmail.com>
2026-09-01 16:37:42 +0530

    Implement XvoxAppRoot composable function

---
+++ app/src/main/java/com/xvox/music/XvoxAppRoot.kt
@@ -0,0 +1,32 @@
+package com.xvox.music
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.xvox.music.features.home.HomeScreen
+import com.xvox.music.features.setup.SetupScreen
+
+@Composable
+fun XvoxAppRoot(
+    viewModel: AppViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsState()
+
+    when (state) {
+        AppUiState.Loading -> Unit
+
+        AppUiState.Setup -> {
+            SetupScreen(
+                onSetupComplete = {
+                    // DataStore emission changes
+                    // AppUiState automatically.
+                }
+            )
+        }
+
+        AppUiState.Home -> {
+            HomeScreen()
+        }
+    }
+}
