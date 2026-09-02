90835c0a93bc48b334d72dea752e495397163d98
lxzrvi <lxzrvi@gmail.com>
2026-09-02 14:40:21 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,13 @@ android {
     }
 }
 
+configurations.configureEach {
+    resolutionStrategy {
+        force("androidx.core:core:1.17.0")
+        force("androidx.core:core-ktx:1.17.0")
+    }
+}
+
 dependencies {
     val composeBom = platform(
         "androidx.compose:compose-bom:2025.08.00"
@@ -36,33 +43,35 @@ dependencies {
     implementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.10.1")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2"
+    )
 
-    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    implementation(
+        "androidx.datastore:datastore-preferences:1.1.7"
+    )
 
-    implementation("androidx.media3:media3-exoplayer:1.8.0")
-    implementation("androidx.media3:media3-session:1.8.0")
+    implementation(
+        "androidx.media3:media3-exoplayer:1.8.0"
+    )
 
-    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+    implementation(
+        "androidx.media3:media3-session:1.8.0"
+    )
 
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(
+        "io.coil-kt.coil3:coil-compose:3.3.0"
+    )
 
-    implementation("com.github.skydoves:cloudy:0.7.1")
+    implementation(
+        "androidx.core:core-splashscreen:1.0.1"
+    )
 
-    constraints {
-        implementation("androidx.core:core:1.17.0") {
-            because(
-                "XVOX currently compiles with SDK 36 and AGP 8.13.2"
-            )
-        }
-
-        implementation("androidx.core:core-ktx:1.17.0") {
-            because(
-                "XVOX currently compiles with SDK 36 and AGP 8.13.2"
-            )
-        }
-    }
+    implementation(
+        "com.github.skydoves:cloudy:0.7.1"
+    )
 }
