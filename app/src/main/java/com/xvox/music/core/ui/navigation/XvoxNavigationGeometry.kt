625d08e37d227ff1fbc9b8dea805b4dc84af61fd
lxzrvi <lxzrvi@gmail.com>
2026-09-02 10:56:20 +0530

    Update XvoxNavigationGeometry.kt

--- app/src/main/java/com/xvox/music/core/ui/navigation/XvoxNavigationGeometry.kt
+++ app/src/main/java/com/xvox/music/core/ui/navigation/XvoxNavigationGeometry.kt
@@ -7,9 +7,22 @@ object XvoxNavigationGeometry {
     val barWidth = 246.dp
     val barHeight = 64.dp
 
+    /*
+     * 80dp selector needs room beyond the
+     * 64dp resting parent.
+     *
+     * Extra 20dp gives 10dp above and below.
+     */
+    val hostHeight = 84.dp
+    val hostOverflow = 10.dp
+
     val selectorRestWidth = 78.dp
     val selectorRestHeight = 56.dp
 
+    /*
+     * Hold:
+     * 78x56 -> 110x80
+     */
     val selectorGrowWidth = 32.dp
     val selectorGrowHeight = 24.dp
 
