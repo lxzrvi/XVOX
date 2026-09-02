1cfb63853d8fb196abfdddc26b26b2335f589949
lxzrvi <lxzrvi@gmail.com>
2026-09-02 14:46:46 +0530

    Update compileSdk and refactor dependencies in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.xvox.music"
-    compileSdk = 36
+
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.xvox.music"
@@ -19,8 +19,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
 
     buildFeatures {
@@ -28,24 +31,25 @@ android {
     }
 }
 
-configurations.configureEach {
-    resolutionStrategy {
-        force("androidx.core:core:1.17.0")
-        force("androidx.core:core-ktx:1.17.0")
-    }
-}
-
 dependencies {
-    val composeBom = platform(
-        "androidx.compose:compose-bom:2025.08.00"
-    )
+    val composeBom =
+        platform(
+            "androidx.compose:compose-bom:2025.08.00"
+        )
 
     implementation(composeBom)
 
-    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
 
     implementation(
         "androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2"
