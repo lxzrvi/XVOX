99bbd077ac5cbe815b26307d7e6f6462ebf35972
lxzrvi <lxzrvi@gmail.com>
2026-09-03 00:31:53 +0530

    Create HomeGeometry.kt

---
+++ app/src/main/java/com/xvox/music/features/home/HomeGeometry.kt
@@ -0,0 +1,18 @@
+package com.xvox.music.features.home
+
+import androidx.compose.ui.unit.dp
+
+object HomeGeometry {
+
+    /*
+     * Shared vertical rhythm:
+     *
+     * Header -> All Songs
+     * Heading -> content
+     * All Songs -> Recent heading
+     * Recent heading -> card
+     * Recent card -> indicator
+     * MiniPlayer -> Navbar
+     */
+    val sectionGap = 12.dp
+}
