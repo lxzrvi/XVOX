221ab8bb6a3ece706335ff5082c4e74d47a0db54
lxzrvi <lxzrvi@gmail.com>
2026-09-02 23:05:37 +0530

    Refactor HomeFooter layout and add clipping

--- app/src/main/java/com/xvox/music/features/home/HomeFooter.kt
+++ app/src/main/java/com/xvox/music/features/home/HomeFooter.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -29,26 +29,22 @@ fun HomeFooter(
         XvoxTheme.colors
 
     Column(
-        modifier =
-            modifier,
+        modifier = modifier,
         horizontalAlignment =
             Alignment.CenterHorizontally
     ) {
         Spacer(
             modifier =
-                Modifier.weight(
-                    0.43f
-                )
+                Modifier.weight(0.43f)
         )
 
         Box(
             modifier = Modifier
                 .size(50.dp)
+                .clip(CircleShape)
                 .background(
-                    Color.Black,
-                    CircleShape
-                )
-                .padding(1.dp),
+                    Color.Black
+                ),
             contentAlignment =
                 Alignment.Center
         ) {
@@ -59,17 +55,13 @@ fun HomeFooter(
                     ),
                 contentDescription = null,
                 modifier =
-                    Modifier.size(
-                        48.dp
-                    )
+                    Modifier.size(50.dp)
             )
         }
 
         Spacer(
             modifier =
-                Modifier.height(
-                    5.dp
-                )
+                Modifier.height(5.dp)
         )
 
         Text(
@@ -93,9 +85,7 @@ fun HomeFooter(
 
         Spacer(
             modifier =
-                Modifier.weight(
-                    0.57f
-                )
+                Modifier.weight(0.57f)
         )
     }
 }
