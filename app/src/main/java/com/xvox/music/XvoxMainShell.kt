c78536571956cdb6fd52963b9a7ec9fabcd73e2c
lxzrvi <lxzrvi@gmail.com>
2026-09-02 10:57:22 +0530

    Add offset to padding in XvoxMainShell

--- app/src/main/java/com/xvox/music/XvoxMainShell.kt
+++ app/src/main/java/com/xvox/music/XvoxMainShell.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
@@ -72,6 +73,9 @@ fun XvoxMainShell() {
                 )
                 .padding(
                     bottom = 18.dp
+                )
+                .offset(
+                    y = 20.dp
                 )
         )
     }
