d505338eb7a1883c9d739ccac6a6a3a87d035046
lxzrvi <lxzrvi@gmail.com>
2026-09-03 19:51:05 +0530

    Refactor XvoxNowPlayingControls for cleaner code

--- app/src/main/java/com/xvox/music/player/nowplaying/XvoxNowPlayingControls.kt
+++ app/src/main/java/com/xvox/music/player/nowplaying/XvoxNowPlayingControls.kt
@@ -41,24 +41,20 @@ fun XvoxNowPlayingControls(
                 20,
                 onShuffle
             )
-
             BareControl(
                 R.drawable.ic_xvox_skip_previous,
                 25,
                 onPrevious
             )
-
             PlayControl(
-                isPlaying = isPlaying,
-                onClick = onTogglePlay
+                isPlaying,
+                onTogglePlay
             )
-
             BareControl(
                 R.drawable.ic_xvox_skip_next,
                 25,
                 onNext
             )
-
             BareControl(
                 R.drawable.ic_xvox_repeat,
                 20,
@@ -89,7 +85,10 @@ fun XvoxNowPlayingControls(
             constraints.maxWidth,
             constraints.maxHeight
         ) {
-            placeables.forEachIndexed { index, placeable ->
+            placeables.forEachIndexed {
+                    index,
+                    placeable ->
+
                 val x =
                     (
                         constraints.maxWidth *
@@ -98,10 +97,15 @@ fun XvoxNowPlayingControls(
                         ).toInt()
 
                 val y =
-                    (constraints.maxHeight -
-                        placeable.height) / 2
+                    (
+                        constraints.maxHeight -
+                            placeable.height
+                        ) / 2
 
-                placeable.placeRelative(x, y)
+                placeable.placeRelative(
+                    x,
+                    y
+                )
             }
         }
     }
@@ -129,10 +133,12 @@ private fun BareControl(
         contentAlignment = Alignment.Center
     ) {
         Icon(
-            painter = painterResource(resource),
+            painter =
+                painterResource(resource),
             contentDescription = null,
             tint = colors.primaryText,
-            modifier = Modifier.size(iconSize.dp)
+            modifier =
+                Modifier.size(iconSize.dp)
         )
     }
 }
@@ -149,9 +155,9 @@ private fun PlayControl(
 
     val circle =
         if (darkMode) {
-            Color.White.copy(alpha = 0.15f)
+            Color.Black.copy(alpha = 0.22f)
         } else {
-            Color.Black.copy(alpha = 0.10f)
+            Color.White.copy(alpha = 0.70f)
         }
 
     Box(
@@ -172,13 +178,14 @@ private fun PlayControl(
         contentAlignment = Alignment.Center
     ) {
         Icon(
-            painter = painterResource(
-                if (isPlaying) {
-                    R.drawable.ic_xvox_pause
-                } else {
-                    R.drawable.ic_xvox_play
-                }
-            ),
+            painter =
+                painterResource(
+                    if (isPlaying) {
+                        R.drawable.ic_xvox_pause
+                    } else {
+                        R.drawable.ic_xvox_play
+                    }
+                ),
             contentDescription = null,
             tint = colors.primaryText,
             modifier = Modifier.size(25.dp)
