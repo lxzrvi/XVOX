bc7f891176d5ddb9e88baff4f381322033e07c46
lxzrvi <lxzrvi@gmail.com>
2026-09-03 19:49:16 +0530

    Update XvoxPlayerTransitionMotion.kt

--- app/src/main/java/com/xvox/music/core/ui/miniplayer/XvoxPlayerTransitionMotion.kt
+++ app/src/main/java/com/xvox/music/core/ui/miniplayer/XvoxPlayerTransitionMotion.kt
@@ -1,15 +1,23 @@
 package com.xvox.music.core.ui.miniplayer
 
 import androidx.compose.animation.core.AnimationSpec
-import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.tween
 
 object XvoxPlayerTransitionMotion {
-    const val Duration = 320
+    const val Duration = 245
+
+    private val easing =
+        CubicBezierEasing(
+            0.22f,
+            0.61f,
+            0.36f,
+            1f
+        )
 
     val spec: AnimationSpec<Float>
         get() = tween(
             durationMillis = Duration,
-            easing = FastOutSlowInEasing
+            easing = easing
         )
 }
