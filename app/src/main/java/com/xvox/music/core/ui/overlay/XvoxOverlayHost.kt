f2c08b6d41d0214f0f5d9258409934b96c779c61
lxzrvi <lxzrvi@gmail.com>
2026-09-04 10:46:19 +0530

    Create XvoxOverlayHost.kt

---
+++ app/src/main/java/com/xvox/music/core/ui/overlay/XvoxOverlayHost.kt
@@ -0,0 +1,60 @@
+package com.xvox.music.core.ui.overlay
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun XvoxOverlayHost(
+    controller: XvoxOverlayController,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier =
+            modifier.fillMaxSize()
+    ) {
+        controller.listContent?.let {
+            content ->
+
+            XvoxL(
+                onDismiss =
+                    controller::hideL,
+                modifier =
+                    Modifier.fillMaxSize()
+            ) {
+                content()
+            }
+        }
+
+        controller.boxContent?.let {
+            content ->
+
+            XvoxB(
+                onDismiss =
+                    controller::hideB,
+                modifier =
+                    Modifier.fillMaxSize()
+            ) {
+                content()
+            }
+        }
+
+        controller.popup?.let {
+            message ->
+
+            XvoxP(
+                message = message,
+                onFinished = {
+                    controller.clearPopup(
+                        message.id
+                    )
+                },
+                modifier = Modifier.align(
+                    Alignment.TopCenter
+                )
+            )
+        }
+    }
+}
