beac6a9aa2a8233ab7eb1cc0e975f08b9eb48b30
lxzrvi <lxzrvi@users.noreply.github.com>
2026-09-05 19:18:06 +0000

    fix(imports): add scrollBy import in XvoxMainShell

--- app/src/main/java/com/xvox/music/XvoxMainShell.kt
+++ app/src/main/java/com/xvox/music/XvoxMainShell.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
